995d24cdf73ac06d933d5f2a75c42ae8a6b8b954
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-08-17 03:21:24 +0200

    new placeholder createCourse

--- front/src/admin-pages/Create-course.jsx
+++ front/src/admin-pages/Create-course.jsx
@@ -83,7 +83,7 @@ export default function FormCreateCourse() {
               <input
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
-                placeholder={"titre"}
+                placeholder={"Nom de la course"}
                 name="titre"
                 required
               />
@@ -93,7 +93,7 @@ export default function FormCreateCourse() {
               <input
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
-                placeholder={"date"}
+                placeholder={"Date de la course"}
                 name="date"
                 required
               />
@@ -113,7 +113,7 @@ export default function FormCreateCourse() {
                 <input
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   type="text"
-                  placeholder={"description"}
+                  placeholder={"Description"}
                   name="description"
                   required
                 />
@@ -130,11 +130,11 @@ export default function FormCreateCourse() {
               <input
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
-                placeholder={"adress de depart"}
+                placeholder={"Lieu de départ"}
                 name="adress"
               />
                  <div className="flex justify-center md:col-span-2">
-              <button className ="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"type="submit">Valider l'adresse <br/> (cliqué 2 fois sur la carte pour recentrer sur l'adresse)</button>
+              <button className ="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"type="submit">{"Valider l'adresse"}<br/>{"(cliqué 2 fois sur la carte pour recentrer sur l'adresse)"}</button>
               </div>
               </div>
             </form>
