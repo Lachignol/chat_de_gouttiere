8b5a6c11ec660ce433fa33566cdb26b8913e7098
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-08-17 03:41:19 +0200

    classement apge for all

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -22,18 +22,16 @@ const Navbar = () => {
           <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
             <NavLink to={"/derniere-course/"}>DerniereCourse</NavLink>
           </li>
+            <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              {" "}
+              <NavLink to={"/classement/"}>Classement Page</NavLink>
+            </li>
           {!token && (
             <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
               {" "}
               <NavLink to={"/login-page/"}>login Page</NavLink>
             </li>
           )}
-              {userRole == "Admin" && (
-                <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  {" "}
-                  <NavLink to={"/classement/"}>Classement Page</NavLink>
-                </li>
-              )}
           {userRole == "Admin" && (
             <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
               {" "}
